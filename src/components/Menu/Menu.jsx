02aaf87d7cc79b962f71024a90c2e29c7f019fb3
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-09-18 22:01:47 +0300

    fix component

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,17 +1,24 @@
 import "./Menu.css";
 import { NavLink } from "react-router-dom";
-export default function Menu({closeMenu}){
-    
-
-    return(<>
-        <button className="menu__exit" onClick={closeMenu}></button>
-        <div className="menu">
-            
-            <h2 className="menu__title">Главная</h2>
-                        <NavLink to="/movies" className="menu__link" onClick={closeMenu}>Фильмы</NavLink>
-            <NavLink to="/saved-movies" className="menu__link" onClick={closeMenu}>Сохранённые фильмы</NavLink>
-            <NavLink to="/profile"><button className="menu__acaunt" onClick={closeMenu}>Аккаунт</button></NavLink>
-            <div className="menu__icon"></div>
-        </div></>
-    )
+export default function Menu({ closeMenu }) {
+  return (
+    <div className="menu">
+      <button className="menu__exit" onClick={closeMenu}></button>
+      <div className="menu__container">
+      <NavLink className="menu__link" to="/">Главная</NavLink>
+        <NavLink to="/movies" className="menu__link" onClick={closeMenu}>
+          Фильмы
+        </NavLink>
+        <NavLink to="/saved-movies" className="menu__link" onClick={closeMenu}>
+          Сохранённые фильмы
+        </NavLink>
+        <NavLink to="/profile">
+          <button className="menu__acaunt" onClick={closeMenu}>
+            Аккаунт
+          </button>
+        </NavLink>
+        <div className="menu__icon"></div>
+      </div>
+    </div>
+  );
 }
