d80bbd9f03f4345456c8dde77ed021d8e3d7e380
nUc1eaAr-gampRUS545 <n-mutovin2003@mail.ru>
2023-09-22 00:08:19 +0300

    ....

--- src/components/Main/Portfolio/Portfolio.jsx
+++ src/components/Main/Portfolio/Portfolio.jsx
@@ -13,14 +13,14 @@ export default function Portfolio() {
             Студент МГТУ им. Баумана
           </p>
           <p className="portfolio__container__history">
-            Я родился и живу в Саратове, закончил факультет экономики СГУ. У
-            меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь
-            бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ
+            Я родился и живу в Белёве, закончил факультет экономики КГУ. У
+            меня есть жена и дочь. Я люблю мужчин, а ещё увлекаюсь
+            качалкой. Недавно начал кодить, хочу стать фронтендером. С 2015 года работал в компании «СКБ
             Контур». После того, как прошёл курс по веб-разработке, начал
             заниматься фриланс-заказами и ушёл с постоянной работы.
           </p>
-          <p className="portfolio__container__git">Git</p>
-          <p className="portfolio__container__info">Портфолио</p>
+          <a className="portfolio__container__git" href="https://github.com/nUc1eaAr-gampRUS545/">Git  nUc1eaAr-gampRUS545</a>
+          <a className="portfolio__container__info" href="https://kaluga.hh.ru/applicant/resumes?hhtmFrom=main&hhtmFromLabel=header">Портфолио</a>
         </div>
       </div>
     
